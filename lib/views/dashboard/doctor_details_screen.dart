5f26ff480b3adf05f67dd9d15cdf0ac62e9b33a0
samir shrestha <shresthasammir@gmail.com>
2023-09-10 08:02:46 +0545

    commit

--- lib/views/dashboard/doctor_details_screen.dart
+++ lib/views/dashboard/doctor_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:fileformatting/controllers/dashboard/doctors_screen_controller.dart';
 import 'package:fileformatting/models/all_doctors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
